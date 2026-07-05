15e7186e2cc986a65f8f3652e761957031932728
Tom Marcorelli <tommarcorelli@gmail.com>
2026-07-05 19:02:22 +0200

    Fix icones transparentes + cache v18

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Le cache est invalidé automatiquement quand script.js ou style.css changent
 // (network-first sur les assets principaux, cache-first sur les icônes)
 
-const CACHE_NAME = "revision-it-v17";
+const CACHE_NAME = "revision-it-v18";
 const STATIC_ASSETS = [
   "./index.html",
   "./style.css",
